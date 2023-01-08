37dac73572438eda05463fcbfefdbdb88abb8098
Johannes W <johanneswiden2003@gmail.com>
2023-01-08 21:23:20 +0100

    movable boxes can push each other

--- CoopPuzzle/GameObjects/MovableBlock.cs
+++ CoopPuzzle/GameObjects/MovableBlock.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,12 +29,13 @@ namespace CoopPuzzle
             sb.Draw(tex, hitbox, null, color, 0f, Vector2.Zero, SpriteEffects.None, depth);
         }
 
-        public void Push(Player player, List<GameObject> objects, out bool stuck)
+        public void Push(GameObject pusher, List<GameObject> objects, out bool stuck)
         {
-            Vector2 up = new Vector2(0, player.hitbox.Top - hitbox.Bottom);
-            Vector2 down = new Vector2(0, player.hitbox.Bottom - hitbox.Top);
-            Vector2 left = new Vector2(player.hitbox.Left - hitbox.Right, 0);
-            Vector2 right = new Vector2(player.hitbox.Right - hitbox.Left, 0);
+            stuck = false;
+            Vector2 up = new Vector2(0, pusher.hitbox.Top - hitbox.Bottom);
+            Vector2 down = new Vector2(0, pusher.hitbox.Bottom - hitbox.Top);
+            Vector2 left = new Vector2(pusher.hitbox.Left - hitbox.Right, 0);
+            Vector2 right = new Vector2(pusher.hitbox.Right - hitbox.Left, 0);
             Vector2[] vectors = new Vector2[] { up, down, left, right };
             IEnumerable<Vector2> sortedVectors = vectors.OrderBy(v => v.Length());
             vectors = sortedVectors.ToArray();
@@ -44,15 +46,27 @@ namespace CoopPuzzle
             {
                 if (objects[i] == this || objects[i] is WeighedSwitch)
                     continue;
-
                 if (hitbox.Intersects(objects[i].hitbox))
                 {
+                    if (objects[i] is MovableBlock)
+                    {
+                        MovableBlock mb = (MovableBlock)objects[i];
+                        mb.Push(this, objects, out stuck);
+                        if (stuck)
+                            Pos -= vectors[0] * 0.75f;
+                        return;
+                    }
+                    else if (objects[i] is Door)
+                    {
+                        Door door = (Door)objects[i];
+                        if (door.Open)
+                            continue;
+                    }
                     Pos -= vectors[0] * 0.75f;
                     stuck = true;
                     return;
                 }
             }
-            stuck = false;
         }
     }
 }
