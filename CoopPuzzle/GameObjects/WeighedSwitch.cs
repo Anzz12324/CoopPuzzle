6853a38c5a2199b3e73641f3d3822c6c1a4e0a66
Johannes W <johanneswiden2003@gmail.com>
2023-01-08 21:24:44 +0100

    fix weighedswitch hitbox again

--- CoopPuzzle/GameObjects/WeighedSwitch.cs
+++ CoopPuzzle/GameObjects/WeighedSwitch.cs
@@ -10,7 +10,7 @@ namespace CoopPuzzle
     {
         public bool Weight { get; private set; }
         public int id { get; private set; }
-        public override Rectangle hitbox { get { return new Rectangle((int)position.X + 4, (int)position.Y + 6, (int)size.X - 4 * 2, (int)size.Y - 8); } }
+        public override Rectangle hitbox { get { return new Rectangle((int)position.X + 4, (int)position.Y + 6, (int)size.X - 4 * 2, (int)size.Y - 16); } }
 
         public WeighedSwitch(Vector2 position, Color color, int id) : base(position, color)
         {
@@ -33,7 +33,7 @@ namespace CoopPuzzle
 
         public override void Draw(SpriteBatch sb)
         {
-            //sb.Draw(Assets.white, hitbox, Color.Red);
+            sb.Draw(Assets.white, hitbox, Color.Red);
             if (Weight)
             {
                 sb.Draw(Assets.undertaleRuins, Pos, new Rectangle(23, 837, 20, 20), Color.White, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
