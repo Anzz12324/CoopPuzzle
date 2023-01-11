34e82b144e467e2e9406fa6b12637d1be117f68c
Andreas Rödin <an4507@mau.se>
2023-01-11 12:47:00 +0100

    NPC

--- CoopPuzzle/Npc/HintNpc.cs
+++ CoopPuzzle/Npc/HintNpc.cs
@@ -13,22 +13,35 @@ namespace CoopPuzzle.Npc
             tex = Assets.mettaton;
             frameCount = 2;
             srcRecArray = new Rectangle[frameCount];
-            srcRecArray[0] = new Rectangle(9,324,52,46);
+            srcRecArray[0] = new Rectangle(9, 324, 52, 46);
             srcRecArray[1] = new Rectangle(62, 324, 58, 46);
             bubbleSrcRec = new Rectangle(20, 315, 174, 105);
-            range = new Rectangle((int)pos.X-10, (int)pos.Y+50, 120, 100);
-            bubblePos = new Vector2(pos.X - (bubbleSrcRec.Width / 2) + srcRecArray[0].Width-5, pos.Y - bubbleSrcRec.Height);
+            range = new Rectangle((int)pos.X - 10, (int)pos.Y + 50, 120, 100);
+            bubblePos = new Vector2(pos.X - (bubbleSrcRec.Width / 2) + srcRecArray[0].Width - 5, pos.Y - bubbleSrcRec.Height);
             textPos = new Vector2(bubblePos.X + 5, bubblePos.Y + 5);
             color = Color.Black;
             dialogueArray = new string[][]
             {
             new string[]
             {
-                "Lorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum",
-                "Lorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum",
-                "Lorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum",
-                "Lorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum",
-                "Lorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum\nLorem ipsum Lorem ipsum"
+                "             Hey you. \nDid you know that the \nbutton name came from\nthe french word 'bouton'",
+                "Buttons have been\nproduced from almost\nevery known manmade\nand natural material.",
+                "Did you know that\nsome of the most\n-feared buttons\naren't real. WOW",
+                "But our boutons are\namazing they open doors.\nThat is cool. Now try\nstepping on the button."
+            },
+            new string[]
+            {
+                "Did you see that. WOW\nOur buttons are amazing\nTo bad that they are\nSuper expensive",
+                "or I would have \n1 000 000 000 of them.\nI love me some boutons.\nOOO beautiful boutons.",
+                "               EEYY\nYou don't happen to have\nsome gold on you?\nI need more boutons.",
+                "Owell time for you\nto move on but we might\nmeet again. See you\nlater. boutons boutons",
+            },
+            new string[]
+            {
+                "Did you see that. WOW\nOur buttons are amazing\nTo bad that they are\nSuper expensive",
+                "or I would have \n1 000 000 000 of them.\nI love me some boutons.\nOOO beautiful boutons.",
+                "               EEYY\nYou don't happen to have\nsome gold on you?\nI need more boutons.",
+                "Owell time for you\nto move on but we might\nmeet again. See you\nlater. boutons boutons",
             }
             };
             hitbox = new Rectangle((int)pos.X, (int)pos.Y + (srcRecArray[0].Height * 2 - 10), srcRecArray[0].Width * 2, 10);
@@ -41,7 +54,7 @@ namespace CoopPuzzle.Npc
             if (playerInRange)
             {
                 sb.Draw(bubbleTex, bubblePos, bubbleSrcRec, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, depth);
-                sb.DrawString(Assets.font, dialogueArray[dialogueNum][textNum], textPos , color, 0, Vector2.Zero, 1, SpriteEffects.None, depth + 0.01f);
+                sb.DrawString(Assets.font, dialogueArray[dialogueNum][textNum], textPos, color, 0, Vector2.Zero, 1, SpriteEffects.None, depth + 0.01f);
             }
 
             base.Draw(sb);
--- CoopPuzzle/Npc/StoryNpc.cs
+++ CoopPuzzle/Npc/StoryNpc.cs
@@ -17,8 +17,8 @@ namespace CoopPuzzle.Npc
             srcRecArray[0] = new Rectangle(5, 17, 21, 21);
             srcRecArray[1] = new Rectangle(29, 17, 21, 21);
             bubbleSrcRec = new Rectangle(20, 315, 174, 105);
-            range = new Rectangle((int)pos.X-30, (int)pos.Y-30, 100, 100);
-            bubblePos = new Vector2(pos.X - (bubbleSrcRec.Width / 2) + srcRecArray[0].Width+2, pos.Y - bubbleSrcRec.Height);
+            range = new Rectangle((int)pos.X - 30, (int)pos.Y - 30, 100, 100);
+            bubblePos = new Vector2(pos.X - (bubbleSrcRec.Width / 2) + srcRecArray[0].Width + 2, pos.Y - bubbleSrcRec.Height);
             textPos = new Vector2(bubblePos.X + 5, bubblePos.Y + 10);
             color = Color.Black;
             dialogueArray = new string[][]
@@ -29,6 +29,10 @@ namespace CoopPuzzle.Npc
                     "What's that? You and\nyour friend have an\nincredibly important\nletter for the king?",
                     "It has to be delivered\nby dawn or else the\nwhole kingdom will be in\ndanger?",
                     "Alright, head south to\nenter the castle."
+                },
+                new string[] 
+                {
+                    ""
                 }
             };
             hitbox = new Rectangle((int)pos.X, (int)pos.Y + (srcRecArray[0].Height * 2 - 10), srcRecArray[0].Width * 2, 10);
@@ -40,9 +44,9 @@ namespace CoopPuzzle.Npc
             if (playerInRange)
             {
                 sb.Draw(bubbleTex, bubblePos, bubbleSrcRec, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, depth);
-                sb.DrawString(Assets.font, dialogueArray[dialogueNum][textNum], textPos, color, 0, Vector2.Zero, 1, SpriteEffects.None, depth+0.01f);
+                sb.DrawString(Assets.font, dialogueArray[dialogueNum][textNum], textPos, color, 0, Vector2.Zero, 1, SpriteEffects.None, depth + 0.01f);
             }
-            
+
             base.Draw(sb);
         }
     }
